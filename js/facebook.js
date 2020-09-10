c0bf392dc58f0d409f4ede579c00ae8fdb11ff6b
JulianVogt <julian.vogt1921@gmail.com>
2020-09-10 12:21:09 +1000

    Initial Commit

--- js/facebook.js
+++ js/facebook.js
@@ -45,30 +45,3 @@ window.fbAsyncInit = function() {
                 }
             });
         }
-        //Still logged in
-function stillLoggedIn(){
-    FB.getLoginStatus(function(response) {
-        if (response.status === 'connected') {
-            // The user is logged in and has authenticated your
-            // app, and response.authResponse supplies
-            // the user's ID, a valid access token, a signed
-            // request, and the time the access token
-            // and signed request each expire.
-            var uid = response.authResponse.userID;
-            var accessToken = response.authResponse.accessToken;
-            console.log("Logged In");
-        } else if (response.status === 'not_authorized') {
-            // The user hasn't authorized your application.  They
-            // must click the Login button, or you must call FB.login
-            // in response to a user gesture, to launch a login dialog.
-            console.log("Unauthorized. Logout proceeding");
-            window.location.replace("julianvogt.github.io/GaPS/index.html")
-        } else {
-            // The user isn't logged in to Facebook. You can launch a
-            // login dialog with a user gesture, but the user may have
-            // to log in to Facebook before authorizing your application.
-            console.log("Not logged in. Redirecting to login page.");
-            window.location.replace("julianvogt.github.io/GaPS/index.html")
-        }
-    });
-}
